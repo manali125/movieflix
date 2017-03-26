0e89a98c84a29c2ac2cf4397a9bd6317d04ffaa8
manali125 <mss92@njit.edu>
2017-03-26 17:59:45 -0400

    added partials & views

--- v1/app.js
+++ v1/app.js
@@ -1,14 +1,40 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
+var movies =[{title:"Star Wars",rating:4,description:"Lorem Ipsum Lorem Ipsum"},
+             {title:"Beauty and the Beast",rating:5,description:"Lorem Ipsum Lorem Ipsum"},
+             {title:"Fast and furious",rating:3,description:"Lorem Ipsum Lorem Ipsum"}
+     ];
+
+
+
 app.get("/",function(req,res){
-    res.render("views/show");
+     res.render("landing.ejs");
 });
 
+app.get("/movies",function(req,res){
+     res.render("movies.ejs",{movies:movies});
+});
 
+app.post("/movies",function(req,res){
+	//get form data and add to movies page
+	var title = req.body.title;
+	var rating = req.body.rating;
+	var description = req.body.description;
+	var newMovie = {title:title,rating:rating,description:description};
+	movies.push(newMovie);
+	//redirect to movies page
+    res.redirect("/movies");
+});
+
+app.get("/movies/new",function(req,res){
+  res.render("new.ejs");
+});
 
 app.listen(8080,function() {
-	console.log("Movieflix running on port 8080");
+	console.log("Movieflix running at 8080");
 });
